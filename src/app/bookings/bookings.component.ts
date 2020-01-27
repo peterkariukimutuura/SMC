21bf2dade21dfcd5eeec708c236ec689223aca2d
peterkariukimutuura <peterkariukimutuura@gmail.com>
2020-01-27 12:58:40 +0300

    updates

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -3,24 +3,19 @@ import { Component, OnInit } from '@angular/core';
 
 
 
-export interface PeriodicElement {
+export interface Booking {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  contact: string;
+  time: string;
+  numberofpassengers: number;
+  price: number;
+  rating: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: Booking[] = [
+  {name: "Peter Kariuki Mutuura", contact: "0712169695", time: "23:34", numberofpassengers: 56, price: 43,rating: 67},
+  {name: "James Njogu", contact: "0712169695", time: "23:34", numberofpassengers: 56, price: 43,rating: 67},
+  {name: "Steve Katiechi", contact: "0712169695", time: "23:34", numberofpassengers: 56, price: 43,rating: 67}
 ];
 
 
@@ -38,7 +33,7 @@ export class BookingsComponent implements OnInit {
   // ]
   constructor() { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'contact', 'time', 'numberofpassengers','price','rating'];
   dataSource = ELEMENT_DATA;
 
   ngOnInit() {
